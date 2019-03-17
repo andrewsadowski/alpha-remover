82af7249ecef77893fc38bd597db2bb93c139491
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-03-16 22:20:11 -0700

    Updated main script with image loop

--- src/index.js
+++ src/index.js
@@ -16,3 +16,33 @@ const depCheck = () => {
 module.exports = {
   main
 };
+/**
+ 
+ 
+# make sure to have brew installed, see https://brew.sh:
+#   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+# make sure to have imagemagick installed, see https://imagemagick.org:
+#   brew install imagemagick
+#
+
+if command -v convert; then
+    echo "imagemagick seems to be installed"
+else
+    echo "imagemagick not installed, trying to install ..."
+    if command -v brew; then
+        echo "brew is installed, using it"
+    else
+        echo "brew not installed, trying to install ..."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    brew install imagemagick
+fi
+
+for i in `ls res/icon/ios/*.png`;
+do
+    echo "convert $i"
+    convert $i -background white -alpha remove -alpha off $i;
+done
+*/
