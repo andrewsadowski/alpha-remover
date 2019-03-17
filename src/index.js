cdfb7c5357121660cf323dcc028ca3a636f4458c
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-03-16 20:26:36 -0700

    Added depCheck method to check for imagemagic dep

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 const shell = require("shelljs");
+
+/**
+ * Function checks if necessary dependencies are installed
+ */
+const depCheck = () => {
+  let isIMinstalled;
+  if (shell.exec("imagemagick -v").code !== 0) {
+    shell.echo("imageMagic is not installed");
+    isIMinstalled = false;
+    shell.exit(1);
+  }
+  return isIMinstalled;
+};
+
+module.exports = {
+  main
+};
